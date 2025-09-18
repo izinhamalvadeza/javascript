da7ff9a7a5b5d45e0d2a41cd04bfde1395100456
izinhamalvadeza <162621978+izinhamalvadeza@users.noreply.github.com>
2025-09-18 18:24:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+document
+.getElementById("texto").innerHTML = 
+"Meu primeiro texto <b>JS</b>!";
+
+alert(10 + 5);
+console.log("Oi isso é um alerta");
+let a,b,c; 
+a=5; 
+b=6; 
+c=7
+let pessoa = "Dimitri";
+
+function minhafuncao(){
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+    document.write(5 + 5);
+}
+
+
